f81f9c5f38a9bb5b395903d492cc88943e90f3fa
vmrepo <crash221b@gmail.com>
2021-08-29 20:05:45 -0700

    did more shit

--- dist/default/production/compiler_support.d
+++ dist/default/production/compiler_support.d
@@ -1,2 +1,2 @@
 dist/default/production\compiler_support.p1: \
-  C:\Compilers\Microchip\xc8\v2.32\pic\sources\c99\pic\compiler_support.c
+  C:\Compiler\Microchip\pic\sources\c99\pic\compiler_support.c
--- main.c
+++ main.c
@@ -17,7 +17,21 @@ union PORTDx
         unsigned char rb6:1;   
         unsigned char rb7:1;                                
     }PORTDBits;                  
-    unsigned char registers;   
+    unsigned char registersD;   
+};       
+union PORTBx            
+{                          
+    struct {  
+        unsigned char rb0:1;
+        unsigned char rb1:1;
+        unsigned char rb2:1;
+        unsigned char rb3:1;
+        unsigned char rb4:1;   
+        unsigned char rb5:1;   
+        unsigned char rb6:1;   
+        unsigned char rb7:1;                                
+    }PORTBBits;                  
+    unsigned char registersB;   
 };       
 
 
@@ -40,19 +54,26 @@ int testPort(int *trisPort, int *Port, int direction, int direction2, int counte
 
 void main(){ 
     unsigned int* PORTDx = (unsigned int*)0x0F;
+    unsigned int* PORTBx = (unsigned int*)0x0D;
     unsigned int* TRISDx = (unsigned int*)0x8F;
+    unsigned int* TRISBx = (unsigned int*)0x8D;
     unsigned int* PORTx = PORTDx;
     unsigned int* TRISx = TRISDx;
-    *TRISx = 1;
+    *TRISBx = 0;
+    *TRISx = 0;
     union PORTDx d;
-    d.registers = 0;
-    *PORTx = d.PORTDBits.rb3 = 1;
+    union PORTBx b;
+    *PORTx = d.PORTDBits.rb3= 1;
+    *PORTBx = b.PORTBBits.rb3 = 1;
     while(1)
     {    
-       *PORTx = d.registers;
+       *PORTBx = b.registersB;
+       *PORTx = d.registersD;
        _delay(50000);
        *PORTx = 0;
-       d.registers >>= 1;     
+       *PORTBx = 0;
+       d.registersD >>= 1;    
+       b.registersB >>= 1;
     }
 }                         
                         
